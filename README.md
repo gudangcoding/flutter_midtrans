bab18bd501350838abae05decb1e5edada5de138
gudangcoding <dientrimedia@gmail.com>
2024-02-19 02:28:01 +0700

    komit1

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_midtrans
+
+A new Flutter project and midtrans.
+
+## url plugin
+
+https://pub.dev/packages/midtrans_snap
+
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:midtrans_snap/midtrans_snap.dart';
+import 'package:midtrans_snap/models.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: MidtransSnap(
+            mode: MidtransEnvironment.sandbox,
+            token: 'TOKEN MIDTRANS',
+            midtransClientKey: 'KLIEN_KEY',
+            onPageFinished: (url) {
+              print(url);
+            },
+            onPageStarted: (url) {
+              print(url);
+            },
+            onResponse: (result) {
+              print(result.toJson());
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
